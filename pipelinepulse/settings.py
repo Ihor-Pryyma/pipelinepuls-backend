fc9e35e4b59580020ec3fff1d509231afd217a43
Ihor Pryyma <ih.pryyma@gmail.com>
2023-12-30 23:27:57 +0200

    conf: add eslint pre-commit settings

--- pipelinepulse/settings.py
+++ pipelinepulse/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3*)kbk7zsgz-sbv!7q*ggtc1g+(1d80@r+8#5l4wkirzv47mil'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
