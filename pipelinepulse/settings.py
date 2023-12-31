1c83a3e753783a907d5f92a58dcfca22e0910431
Ihor Pryyma <83470037+Ihor-Pryyma@users.noreply.github.com>
2023-12-31 12:17:09 +0200

    feat: migrate to use postgresql db (#8)

--- pipelinepulse/settings.py
+++ pipelinepulse/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pipelinepulse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coredb',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
